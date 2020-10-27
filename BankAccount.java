b681c430edc7df2d6719b335884d143cb1aa2e31
Aries <aho11@stuy.edu>
2020-10-27 18:48:29 -0400

    created and implemented constructor

--- BankAccount.java
+++ BankAccount.java
@@ -2,4 +2,10 @@ public class BankAccount{
   private double balance;
   private int accountID;
   private String password;
+
+  //constructor
+  public BankAccount(int accountID, String password){
+    accountID = accountID;
+    password = password;
+  }
 }
