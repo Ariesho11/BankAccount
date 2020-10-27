c62637ce4d0317ad522a12e596514a5177addb6f
Aries <aho11@stuy.edu>
2020-10-27 19:32:15 -0400

    tested the constructor

--- Tester.java
+++ Tester.java
@@ -1,3 +1,7 @@
 public class Tester{
-  
+  public static void main(String[]args){
+    BankAccount ba = new BankAccount(2345, "password");
+    System.out.println("Account Balance: " + ba.getBalance());
+    System.out.println("Account ID " + ba.getAccountID());
+  }
 }
